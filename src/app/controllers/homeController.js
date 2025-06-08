4b91dcbf498c773619a5e4e7f3c578310adb130b
nguyendinhminhkhanh <mr.dinhkhanh18@gmail.com>
2025-06-08 16:20:57 +0700

    update parameter trademark

--- src/app/controllers/homeController.js
+++ src/app/controllers/homeController.js
@@ -1,18 +1,28 @@
 const Product = require("../models/Products");
+const Trademark = require("../models/Trademarks");
 const { mongooseToObject } = require("../../util/mongose");
 const { mutipleMongooseToObject } = require("../../util/mongose");
 class homeController {
-    async show(req, res) {
-        try {
-            const products = await Product.find()
-                .sort({ createdAt: -1 })
-                .limit(4)
-                .lean(); // Dùng lean() để lấy object thuần
-            res.render("home", { products, BASE_URL: process.env.BASE_URL });
-        } catch {
-            res.status(500).send("Loi server");
-        }
+  async show(req, res) {
+    try {
+
+      const trademarks = await Trademark.find()
+        .sort({ createdAt: -1 })
+        .limit(4)
+        .lean();
+      const products = await Product.find()
+        .sort({ createdAt: -1 })
+        .limit(4)
+        .lean(); // Dùng lean() để lấy object thuần
+      res.render("home", {
+        products,
+        trademarks,
+        BASE_URL: process.env.BASE_URL,
+      });
+    } catch {
+      res.status(500).send("Loi server");
     }
+  }
 }
 
 module.exports = new homeController();
