1749596debf58b828dd3f62d040b9314fd92e895
Nguyen Dinh Minh Khanh <mr.dinhkhanh18@gmail.com>
2025-06-09 22:33:43 +0700

    update product information to database

--- src/app/controllers/homeController.js
+++ src/app/controllers/homeController.js
@@ -5,7 +5,6 @@ const { mutipleMongooseToObject } = require("../../util/mongose");
 class homeController {
   async show(req, res) {
     try {
-
       const trademarks = await Trademark.find()
         .sort({ createdAt: -1 })
         .limit(4)
@@ -14,9 +13,33 @@ class homeController {
         .sort({ createdAt: -1 })
         .limit(4)
         .lean(); // Dùng lean() để lấy object thuần
+      const mainboard = await Product.find({
+        category: { $regex: "Mainboard" },
+      });
+      const CPU = await Product.find({
+        category: { $regex: "CPU" },
+      });
+      const RAM = await Product.find({
+        category: { $regex: "RAM" },
+      });
+      const GPU = await Product.find({
+        category: { $regex: "GPU" },
+      });
+      const PSU = await Product.find({
+        category: { $regex: "PSU" },
+      });
+      const SSD = await Product.find({
+        category: { $regex: "SSD" },
+      });
       res.render("home", {
         products,
         trademarks,
+        mainboard: mutipleMongooseToObject(mainboard),
+        CPU: mutipleMongooseToObject(CPU),
+        RAM: mutipleMongooseToObject(RAM),
+        GPU: mutipleMongooseToObject(GPU),
+        PSU: mutipleMongooseToObject(PSU),
+        SSD: mutipleMongooseToObject(SSD),
         BASE_URL: process.env.BASE_URL,
       });
     } catch {
